ba2765e9d9aba6f24f13ece464f1369d637bdfbf
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-03-25 17:07:21 +0100

    update

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -2,9 +2,7 @@ const { expect } = require('chai');
 describe('Testing numbers', () => {
     it('1 is equal to 1', () => {
         expect(1 === 1).to.be.true;
-
     });
-
     it('2 is equal to 2', () => {
         expect(2 === 2).to.be.true;
     });
@@ -23,4 +21,7 @@ describe('Testing numbers', () => {
     it('6 is equal to 6', () => {
         expect(6 === 6).to.be.true;
     });
+    it('7 is equal to 7', () => {
+        expect(7 === 7).to.be.true;
+      });
 });
