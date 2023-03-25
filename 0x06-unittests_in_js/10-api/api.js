5058a2bd853f8c4b9ae33448f5de5c5128c38be1
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-03-25 20:43:50 +0100

    Deep equality & Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const port = 7865;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+});
+
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+    res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+    const data = {
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      }
+    res.send(data);
+});
+app.post('/login', (req, res) => {
+    const username = req.body.userName;
+    res.send(`Welcome ${username}`)
+});
+
+app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`)
+});
+
