665c4eb8337ea3e53ad4c761a088e993ecf6619c
Solomon Eniola Adegoke <Souceking@gmail.com>
2023-03-25 20:16:05 +0100

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,42 @@
+const { expect } = require('chai');
+const request = require('request');
+
+
+describe("api index page", () => {
+  it("test status code", (done) => {
+    request.get('http://localhost:7865/', (err, res, body) => {
+      if (res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      }
+    });
+  });
+  it("test correct output", (done) => {
+    request.get('http://localhost:7865/', (err, res, body) => {
+      if (body) {
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      }
+    });
+  });
+});
+
+describe("api cart page", function() {
+    it("test valid number param", (done) => {
+        request.get('http://localhost:7865/cart/12', (err, res, body) => {
+            if(res && body) {
+                expect(res.statusCode).to.equal(200);
+                expect(body).to.equal('Payment methods for cart 12');
+                done();
+            }
+        });
+    });
+    it("test non-number param", function(done) {
+        request.get('http://localhost:7865/cart/hello', (err, res, body) => {
+            if(res) {
+                expect(res.statusCode).to.equal(404);
+                done();
+            }
+        });
+    });
+});
